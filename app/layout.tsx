2c762d491d0c2ac63affaae104c57a122402bed9
Claryte <carllaw.banuag@gmail.com>
2025-04-16 18:39:00 +0800

    Fixed dashboard styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,41 +1,15 @@
-"use client"; 
-
 import "@/app/globals.css";
 import { Inter } from "next/font/google";
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
-import { useEffect, useState } from "react";
-import type { Session } from "@supabase/auth-helpers-nextjs"; // Import Session type
 import { ThemeProvider } from "@/components/providers/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
+// This is the root layout that will be server-side rendered
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const supabase = createClientComponentClient();
-  const [session, setSession] = useState<Session | null>(null);
-
-  useEffect(() => {
-    const getSession = async () => {
-      const { data } = await supabase.auth.getSession();
-      setSession(data.session);
-    };
-
-    getSession();
-
-    const { data: authListener } = supabase.auth.onAuthStateChange(
-      async (_event, session) => {
-        setSession(session);
-      }
-    );
-
-    return () => {
-      authListener.subscription.unsubscribe();
-    };
-  }, [supabase]);
-
   return (
     <html lang="en" suppressHydrationWarning>
       <head />
